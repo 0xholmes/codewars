0e2777d72385d7f2ad36d5a8d183c697cd7f2a47
stvAi <71206886+stvAi@users.noreply.github.com>
2021-08-29 09:28:38 +0700

    Update Training JS #14: Methods of Number object--toString() and toLocaleString().js

--- 8 kyu/Training JS #14: Methods of Number object--toString() and toLocaleString().js
+++ 8 kyu/Training JS #14: Methods of Number object--toString() and toLocaleString().js
@@ -1,3 +1,14 @@
+// Solution 1
 const hex = (int) => `${int < 16 ? "0" : ""}${int.toString(16)}`
-
 const colorOf = (r, g, b) => `#${hex(r)}${hex(g)}${hex(b)}`
+
+
+// Solution 2
+function colorOf(r,g,b){
+  return "#" + hex(r) + hex(g) + hex(b)
+  
+  function hex(n) {
+    const result = n.toString(16)
+    return result.length < 2 ? "0" + result : result
+  }
+}
