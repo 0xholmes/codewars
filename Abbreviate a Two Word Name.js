f24a9e7f765a41ca0ae948f53312cb1a24ae1f7f
stvAi <71206886+stvAi@users.noreply.github.com>
2021-09-03 07:26:42 +0700

    Create Abbreviate a Two Word Name.js

---
+++ Abbreviate a Two Word Name.js
@@ -0,0 +1,16 @@
+// kata: https://www.codewars.com/kata/57eadb7ecd143f4c9c0000a3/javascript
+
+// 1st
+function abbrevName(name){
+  const regex = /\b(\w)/g
+  const matches = name.match(regex)
+  const acronym = matches.join('.').toUpperCase()
+  return acronym
+}
+
+// 2nd
+function abbrevName(name){
+  const words = name.split(" ")
+  const fl = (words[0][0] + "." + words[1][0]).toUpperCase()
+  return fl
+}
