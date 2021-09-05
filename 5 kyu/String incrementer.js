d9c1d112daf4c20b6f8be8292a626c847d325efa
stvAi <71206886+stvAi@users.noreply.github.com>
2021-09-05 09:06:00 +0700

    Update String incrementer.js

--- 5 kyu/String incrementer.js
+++ 5 kyu/String incrementer.js
@@ -10,3 +10,15 @@ function incrementString (str) {
       : numStr
   })
 }
+
+// arrow function
+const incrementString = (str) => {
+  return str.replace(/\d*$/, match => {
+    if (!match) return '1'
+    const numStr = String(parseInt(match) + 1)
+    const diff = match.length - numStr.length
+    return diff > 0
+      ? '0'.repeat(diff) + numStr
+      : numStr
+  })
+}
