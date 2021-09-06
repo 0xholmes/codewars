7393112dfb115f57002b4cb65c132fc0601e203a
stvAi <71206886+stvAi@users.noreply.github.com>
2021-09-06 07:35:39 +0700

    Create Mumbling.js

---
+++ 7 kyu/Mumbling.js
@@ -0,0 +1,6 @@
+// kata: https://www.codewars.com/kata/5667e8f4e3f572a8f2000039/javascript
+
+// Map
+const accum = (s) => {
+  return [...s].map((char, idx) => (char.toUpperCase() + char.toLowerCase().repeat(idx))).join("-")
+}
