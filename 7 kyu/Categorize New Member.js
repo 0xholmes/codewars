4f7c5b52af2687f6dfa59bdfa54c4797dfebfd20
stvAi <71206886+stvAi@users.noreply.github.com>
2021-09-15 08:19:29 +0700

    Update Categorize New Member.js

--- 7 kyu/Categorize New Member.js
+++ 7 kyu/Categorize New Member.js
@@ -30,4 +30,4 @@ const openOrSenior = (data) => {
 }
 
 // one line destructuring
-const openOrSenior = (data) => data.map(([age, handicap]) => age > 54 && handicap > 7 ? 'Senior' : 'Open')
+const openOrSenior = (data) => data.map(([age, handicap]) => (age > 54 && handicap > 7) ? 'Senior' : 'Open')
