f653927d75c69c8327841c35e5214b1ef7ace37a
stvAi <71206886+stvAi@users.noreply.github.com>
2021-09-15 08:14:28 +0700

    Create Categorize New Member.js

---
+++ 7 kyu/Categorize New Member.js
@@ -0,0 +1,30 @@
+// kata: https://www.codewars.com/kata/5502c9e7b3216ec63c0001aa/javascript
+
+// forEach
+const openOrSenior = (data) => {
+  let result = []
+  data.forEach(d => {
+    if (d[0] >= 55 && d[1] > 7) {
+      result.push('Senior')
+    } else {
+      result.push('Open')
+    }
+  })
+  return result
+}
+
+// map
+const openOrSenior = (data) => {
+  return data.map(d => {
+    if (d[0] >= 55 && d[1] > 7) {
+      return 'Senior'
+    } else {
+      return 'Open'
+    }
+  })
+}
+
+// destructuring: [age, handicap] https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Operators/Destructuring_assignment
+const openOrSenior = (data) => {
+  return data.map(([age, handicap]) => (age >= 55 && handicap > 7) ? "Senior" : "Open")
+}
